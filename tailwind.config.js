dd0d9446b7bd3dc5b120d591f4384a609265f035
Hendryanto Fudiko <hendryanto.fudiko@gmail.com>
2022-04-10 10:44:38 +0700

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './lib/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
